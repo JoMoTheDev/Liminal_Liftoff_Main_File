c66f9437452091205285e0b4cedb6b545bb53bc9
JoMoTheDev <adamsygilquotom@gmail.com>
2026-04-22 16:17:19 -0500

    Started getting the Interaction Script working
    The idea is: When the ray hits an object, delete that object, store the value against a maximum value that's public to the scene, then once that max value is reached, activate the animator on the door of the level and play it immediately to allow the player to progress.

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -2,12 +2,26 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void OnCollisionEnter(Collision collision)
+    [SerializeField] private LayerMask interactLayer;
+    [SerializeField] private float raycastDistance = 10f;
+    [SerializeField] private GameObject levelDoor;
+
+    public int collectionMaxAmount;
+
+    private int collectionAmount;
+
+    private void Update()
     {
-        if (CompareTag("Player"))
+        RaycastHit hit;
+        Vector3 direction = transform.TransformDirection(Vector3.forward);
+        
+        if (Input.GetMouseButtonDown(1))
         {
-            Destroy(this.gameObject);
+            if (Physics.Raycast(transform.position, direction, out hit, raycastDistance, interactLayer))
+            {
+                Debug.DrawRay(transform.position, direction * hit.distance, Color.yellow);
+                Debug.Log("That thing is " + hit.distance + " meters away!");
+            }
         }
     }
 }
